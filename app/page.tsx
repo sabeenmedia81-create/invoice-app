2bb15cc9061584555ab303b93ed32c28324dc84e
sabeenmedia81-create <sabeenmedia81@gmail.com>
2026-01-15 22:11:20 +0000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Invoice App</h1>
+      <p>Your invoicing application is running successfully.</p>
+    </main>
+  );
+}
