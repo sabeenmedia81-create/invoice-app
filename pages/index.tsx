cf2d08b0afaf52e8c43474216e3837a3c3b90f96
sabeenmedia81-create <sabeenmedia81@gmail.com>
2026-01-26 22:04:45 +0000

    Delete pages/index.tsx

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
-      <h1>Invoice App</h1>
-      <p>Homepage is working ✅</p>
-    </main>
-  );
-}
