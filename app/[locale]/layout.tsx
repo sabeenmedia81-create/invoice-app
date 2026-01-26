c2c3e56d0b1f4c5931369ef1587e51cdbc42e047
sabeenmedia81-create <sabeenmedia81@gmail.com>
2026-01-26 21:27:26 +0000

    Create layout.tsx

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,13 @@
+export default function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  return (
+    <html lang={params.locale}>
+      <body>{children}</body>
+    </html>
+  );
+}
