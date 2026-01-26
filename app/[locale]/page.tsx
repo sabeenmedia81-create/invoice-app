d22f6e5d7c132afe47317ba2738b5f604686a52d
sabeenmedia81-create <sabeenmedia81@gmail.com>
2026-01-26 21:26:29 +0000

    Create page.tsx

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Invoice App</h1>
+      <p>Your invoicing application is running successfully.</p>
+      <p>Locale route is working.</p>
+    </main>
+  );
+}
