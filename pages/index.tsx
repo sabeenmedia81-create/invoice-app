10418251f62e041e0a48b82fe02008f16b60a273
sabeenmedia81-create <sabeenmedia81@gmail.com>
2026-01-26 21:33:20 +0000

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>Invoice App</h1>
+      <p>Homepage is working ✅</p>
+    </main>
+  );
+}
